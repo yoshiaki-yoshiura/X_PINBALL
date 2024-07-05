398374782db58ec2c1929021c65a398edc4ef01d
吉浦 好晃 <kuruitamae@gmail.com>
2024-07-05 19:43:46 +0900

    UnityのバージョンUP＆AdMobSDKインポート
    旧MacBookでAdMobSDKインポート実行でエラー → UnityバージョンUP＆AdMobSDK再インポートするもエラー → MacBookを買い替えUnityのバージョンを更にUP＆AdMobSDKインポートを再々実行（今ここ）。正直データが壊れてないか不安・・・。

--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -5,9 +5,17 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
-    public Sprite _on;
+    //↓ネットで見つけた「Buttonを押してImageを変更する」のやり方。一応メモで残してるだけ。（適当にやって出来な方のでボツにした）
+    /*public Sprite _on;
     public Sprite _off;
     private bool flg = true;
+
+    public void changeImage()
+    {
+        var img = GetComponent<Image>();
+        img.sprite = (flg) ? _on : _off;
+        flg = !flg;
+    }*/
 
 
     void Start()
@@ -21,8 +29,8 @@ public class ButtonController : MonoBehaviour
         //左回転ボタンをタップすると画像が変わる処理
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            //指定した画像へ切り替える
-            gameObject.GetComponent<Animator>().Play("L1"); //「GetKeyDownの3種の違い」と「アニメーションの繰り返し設定」
+            //指定した画像へ切り替える  （アニメだと挙動がうまくいかない）
+            gameObject.GetComponent<Animator>().Play("L1");      //「GetKeyDownの3種の違い」と「アニメーションの繰り返し設定」
         }
 
         //右回転ボタンをタップすると画像が変わる処理
